9892aed778e06379f6f611a850b36773de2252f4
salominreji0621 <salominrejir@gmail.com>
2025-05-16 15:58:00 +0530

    Create loopin pattern .c

---
+++ loopin pattern .c
@@ -0,0 +1,21 @@
+#include <stdio.n>
+int main()
+{
+    int rows;
+    scanf("%d",&rows);
+    for(int a=1; a<=rows; a++)
+    {
+        for(int b=a; b<rows; b++)
+        {
+            printf(" ");
+        }
+        for(int c=1; c<=rows; c++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    
+    return 0;
+}
